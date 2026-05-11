aec864f79f11219db79a67e9b6dc282e870fdd68
wiwiwoiii <alinaid17@gmail.com>
2026-05-11 16:04:45 +0300

    Урок 9

--- defs.h
+++ defs.h
@@ -88,6 +88,8 @@ typedef struct {
 
 #define FR2SQ(f,r) ( (21 + (f) ) + ( (r) * 10 ) )
 #define SQ64(sq120) (Sq120ToSq64[(sq120)])
+#define POP(b) PopBit(b)
+#define CNT(b) CountBits(b)
 
 /* GLOBALS */
 
@@ -98,5 +100,7 @@ extern int Sq64ToSq120[64];
 
 extern void AllInit();
 extern void PrintBitBoard(U64 bb);
+extern int PopBit(U64* bb);
+extern int CountBits(U64 b);
 
 #endif
