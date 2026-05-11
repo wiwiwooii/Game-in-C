76da29f80c65b682c28edce8051ad5a604535d7f
wiwiwoiii <alinaid17@gmail.com>
2026-05-11 13:18:01 +0300

    Изменения кода согласно уроку 6

--- vice.c
+++ vice.c
@@ -3,5 +3,20 @@
 
 int main() {
 
+	AllInit();
+
+	for (index = 0; index < BRD_SQ_NUM; ++index) {
+		if (index % 10 == 0) printf("\n");
+		printf("%d", Sq120ToSq64[index]);
+	}
+
+	printf("\n");
+	printf("\n");
+
+	for (index = 0; index < 64; ++index) {
+		if (index % 8 == 0) printf("\n");
+		printf("%d", Sq64ToSq120[index]);
+	}
+
 	return 0;
 }
